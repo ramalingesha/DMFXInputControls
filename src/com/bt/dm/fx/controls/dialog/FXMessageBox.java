697b119fda837f18be6aae879bb094d9cdb7f9ff
Ramalingesha <ramalingesh.ml@gmail.com>
2023-01-13 13:31:47 +0530

    Message box height setting and error text color update

--- src/com/bt/dm/fx/controls/dialog/FXMessageBox.java
+++ src/com/bt/dm/fx/controls/dialog/FXMessageBox.java
@@ -18,7 +18,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.StageStyle;
 
 import com.bt.dm.core.i18n.DMMessageLocalizer;
@@ -30,6 +29,8 @@ import com.bt.dm.fx.controls.labels.FXLabelCmp;
 import com.bt.dm.fx.controls.labels.FXLabelCmp.FXLabelCmpBuilder;
 import com.bt.dm.fx.controls.labels.FXMaterialDesignIcon;
 import com.bt.dm.fx.controls.labels.FXMaterialDesignIcon.FXMaterialDesignIconBuilder;
+import com.bt.dm.fx.controls.text.FXTextCmp;
+import com.bt.dm.fx.controls.text.FXTextCmp.FXTextCmpBuilder;
 import com.bt.dm.fx.controls.theme.ControlsTheme;
 import com.bt.dm.fx.controls.utils.DMAlertIconConstants;
 import com.bt.dm.fx.controls.utils.Fonts;
@@ -51,7 +52,6 @@ public class FXMessageBox {
 		private boolean titleI18N = true;
 		private boolean messageI18N = true;
 		private boolean messageEnglishFont;
-		private Double messagePanelHeight;
 
 		public FXMessageBoxBuilder(String message) {
 			this.message = message;
@@ -72,11 +72,6 @@ public class FXMessageBox {
 			return this;
 		}
 		
-		public FXMessageBoxBuilder messagePanelHeight(Double messagePanelHeight) {
-			this.messagePanelHeight = messagePanelHeight;
-			return this;
-		}
-
 		public FXMessageBoxBuilder title(String title, boolean i18n) {
 			this.title = title;
 			this.titleI18N = i18n;
@@ -109,7 +104,6 @@ public class FXMessageBox {
 		this.dialog.setMessageBoxTitle(builder.title, builder.titleI18N);
 		this.dialog.changeAlertIcon(builder.alertType);
 		this.dialog.setEnglishFont(builder.messageEnglishFont);
-		this.dialog.setMessagePanelHeight(builder.messagePanelHeight);
 
 		return FXMessageDialog.getInstance().showAndWait();
 	}
@@ -126,21 +120,13 @@ class FXMessageDialog extends Dialog<ButtonType> {
 	private FXMaterialDesignIcon alertIcon;
 	private PubSubEventHandler handler;
 	private boolean messageEnglishFont;
-	private FXLabelCmp messageCmp;
+	private FXTextCmp errorMessageText;
 
 	public void setMessage(String message, boolean i18n) {
 		this.message.setValue(i18n ? DMMessageLocalizer.getLabel(message)
 				: message);
 	}
 	
-	public void setMessagePanelHeight(Double height) {
-		if (height != null) {
-			this.messageCmp.setPrefHeight(height);
-		} else {
-			this.messageCmp.setPrefHeight(40);
-		}
-	}
-
 	public void setMessageBoxTitle(String messageBoxTitle, boolean i18n) {
 		this.messageBoxTitle.setValue(i18n ? DMMessageLocalizer
 				.getLabel(messageBoxTitle) : messageBoxTitle);
@@ -157,32 +143,37 @@ class FXMessageDialog extends Dialog<ButtonType> {
 	public static FXMessageDialog getInstance() {
 		if (instance == null) {
 			instance = new FXMessageDialog();
+			instance.initErrorText();
 			instance.setAlertIcon(AlertType.INFORMATION);
-			instance.initComponent();
+			instance.initErrorPopup();
 		}
 
 		instance.toggleEnglishFont();
 
 		return instance;
 	}
+	
+	private void initErrorText() {
+		errorMessageText = new FXTextCmp(
+				new FXTextCmpBuilder().wrappingWidth(SizeHelper.SCREEN_SIZE.getWidth() * 0.4));
+		errorMessageText.textProperty().bind(this.message);
+	}
 
 	private void toggleEnglishFont() {
 		if (this.messageEnglishFont) {
-			messageCmp.getStyleClass().add("english-font");
+			errorMessageText.getStyleClass().add("english-font");
 		} else {
-			messageCmp.getStyleClass().remove("english-font");
+			errorMessageText.getStyleClass().remove("english-font");
 		}
 	}
 
-	private void initComponent() {
+	private void initErrorPopup() {
 		DialogPane dialogPane = this.getDialogPane();
 		dialogPane.getStyleClass().add("dm-message-box");
 		this.addRootThemeStyleSheet(dialogPane);
 
 		// remove dialog title bar
 		this.initStyle(StageStyle.UNDECORATED);
-		dialogPane.setMinWidth(SizeHelper.SCREEN_SIZE.getWidth() * 0.3);
-		dialogPane.setMaxWidth(SizeHelper.SCREEN_SIZE.getWidth() * 0.4);
 
 		FXMaterialDesignIconBuilder closeIconBuilder = new FXMaterialDesignIconBuilder(
 				MaterialDesignIcon.CLOSE);
@@ -208,18 +199,12 @@ class FXMessageDialog extends Dialog<ButtonType> {
 		titleBox.getStyleClass().add("title-bar");
 		titleBox.setPadding(new Insets(10));
 
-		messageCmp = new FXLabelCmp(new FXLabelCmpBuilder());
-		messageCmp.textProperty().bind(this.message);
-		messageCmp.setWrapText(true);
-		messageCmp.setTextAlignment(TextAlignment.LEFT);
-		messageCmp.setPadding(new Insets(10, 10, 0, 15));
-
 		BorderPane messagePanel = new BorderPane();
 		messagePanel.getStyleClass().add("message-box");
 		messagePanel.setPadding(new Insets(10));
 
 		messagePanel.setLeft(this.alertIcon);
-		messagePanel.setCenter(messageCmp);
+		messagePanel.setCenter(errorMessageText);
 
 		BorderPane root = new BorderPane();
 		root.setPadding(new Insets(0));
@@ -273,11 +258,14 @@ class FXMessageDialog extends Dialog<ButtonType> {
 		MaterialDesignIcon icon;
 		String iconStyle = String.format("-fx-font-size: %s;",
 				DMAlertIconConstants.ALERT_POPUP_ICON_SIZE);
+		errorMessageText.getStyleClass().remove("kannada-error-red-font");
+		
 		switch (alertType) {
 		case ERROR:
 			icon = MaterialDesignIcon.CLOSE_BOX;
 			iconStyle += String.format("-fx-fill: %s;",
 					DMAlertIconConstants.ERROR_ICON_COLOR);
+			errorMessageText.getStyleClass().add("kannada-error-red-font");
 			break;
 		case WARNING:
 			icon = MaterialDesignIcon.ALERT;
